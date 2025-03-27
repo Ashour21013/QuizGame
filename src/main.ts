76f7a17acb98d8b0b92d097445bbd6bb2e112f74
Paul <paulsertul@gmail.com>
2025-03-27 20:44:50 +0100

    Start Game

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
-import { fetchQuestions, Question } from './modules/questions.js';
+import { displayQuestion } from "./modules/ui.js";
+import { fetchQuestions, Question } from "./modules/questions.js";
 
-//beispiel aufruf um die daten zu holen
 async function init() {
   let questions: Question[] = await fetchQuestions();
   console.log(questions);
+
+  const startQuizButton = document.getElementById('start-quiz');
+  const playerNameInput = document.getElementById('player-name');
+  const playerInputDiv = document.getElementById('player-input');
+  const quizDiv = document.getElementById('quiz-container');
+  let playerName: string = '';
+  if (startQuizButton) {
+      startQuizButton.addEventListener('click', () => {
+          if (playerNameInput instanceof HTMLInputElement && playerNameInput.value.length > 0) {
+          playerName = playerNameInput.value;
+          if (playerInputDiv) {
+              playerInputDiv.style.display = 'none';
+          }
+          if (quizDiv) {
+              quizDiv.style.display = 'block';
+              if(questions.length > 0) {
+                displayQuestion(0, questions);
+              }
+          }
+          } else {
+          alert('Please enter your name!');
+          }
+      });
+  }
+
+
+
 }
 
 init();
